42aafb3ec115db519c73e1683c15287df68b3fb3
syab-syab <syabsyabsyabsyab@gmail.com>
2023-08-05 21:02:27 +0900

    ローカルストレージを使う形で落ち着く

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,24 +1,65 @@
 import React from 'react'
 import CodeSelect from './CodeSelect'
-import CacheTest from './CacheTest'
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { useState } from 'react';
+// import CacheTest from './CacheTest'
+// import { QueryClient, QueryClientProvider } from 'react-query';
+// import { useState } from 'react';
+import useFetch from '../hooks/useFetch';
 
-const queryClient = new QueryClient();
+// const queryClient = new QueryClient();
 
 const Main = () => {
-  const [show, setShow] = useState(true);
+  // const [show, setShow] = useState(true);
+
+  const url = "https://jsonplaceholder.typicode.com/todos"
+
+  const { data, isLoaded, error } = useFetch(url);
+
+  // useFetchで取得したデータ(オブジェクトか配列)をローカルストレージに
+  // JSON.stringifyで文字列にしてから格納
+  // localStorage.setItem("test", JSON.stringify(data))
+
+  // ローカルストレージから格納したデータを取ってくる
+  const testData = localStorage.getItem("test")
+
+  // ローカルストレージから取ってきたデータを配列やオブジェクトに戻す
+  const parseData = JSON.parse(testData)
+
+ 
+
+  // localStorage.setItem("data", data)
+  // localStorage.removeItem('data');
+
+  // ローカルストレージに格納(キー, 値)
+  // localStorage.setItem("myCat", "Tom")
+
+  // ローカルストレージから削除(キー)
+  // localStorage.removeItem('myCat');
+
+  // ローカルストレージから取得(キー)
+  // もし取得できなければnullが帰ってくる
+  // var cat = localStorage.getItem('myCat');
+
+  // ローカルストレージの中の値をすべて削除
+  // localStorage.clear();
 
   return (
-    <QueryClientProvider client={queryClient}>
+    // <QueryClientProvider client={queryClient}>
       <div>
         <CodeSelect />
-        <div>
+        {}
+        {isLoaded && <p>Loading...</p>}
+        {error && <p>Error</p>}
+        { data && <div>
+        {parseData.map((todo) => (
+              <div key={todo.id}>{todo.title}</div>
+            ))}
+          </div>}
+        {/* <div>
           <button onClick={() => setShow(!show)}>Toggle</button>
-        </div>
-        { show && <CacheTest /> }
+        </div> */}
+        {/* { show && <CacheTest /> } */}
     </div>
-    </QueryClientProvider>
+    // </QueryClientProvider>
 
   )
 }
